a9fb9439ef6f640417dd9886e768d85057623962
Nikita.Zmanovskiy <72348175+test212366@users.noreply.github.com>
2023-10-24 19:50:55 +0300

    Add files via upload

---
+++ src/pr123.java
@@ -0,0 +1,17 @@
+
+public class pr123 {
+
+	public static void main(String[] args) {
+		
+		int i = 2;
+        if(i == 1) { // аналог "case 1"
+            System.out.println("Тест работает");
+        } else if(i == 2) { // аналог "case 2"
+            System.out.println("Тест добрый");
+        } else if(i == 3) { // аналог "case 3"
+            System.out.println("Тест поможет вам");
+        }
+
+	}
+
+}
